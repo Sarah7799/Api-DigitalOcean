5fce6f926da1b31d6046368403333bd25b70d4e9
Sarah7799 <82445581+Sarah7799@users.noreply.github.com>
2022-02-01 00:56:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 async function getData() {
     try{
-    let resp = await fetch("https://status.digitalocean.com/api/v2/components.json");
+    let resp = await fetch("https://wandbox.org/api/list.json");
     let data = await resp.json();
     createTable(data)
     return data;
@@ -13,19 +13,21 @@ async function getData() {
 getData();
 
 
+
 function createTable(data){
  let tBody = document.getElementById('tbody')
 
     data.forEach((element) => {
       let row = document.createElement('tr');
       let td1 = document.createElement("td");
-      td1.innerHTML = element.page.id;
+      td1.innerHTML = element.compiler-option-raw;
       let td2 = document.createElement('td');
-      td2.innerHTML=element.page.name;
+      td2.innerHTML=element.runtime-option-raw;
       let td3 = document.createElement('td');
-      td3.innerHTML = element.page.url;
+      td3.innerHTML = element.display-compile-command;
       
       row.append(td1,td2,td3);
       tBody.append(row);
     });
 }
+
