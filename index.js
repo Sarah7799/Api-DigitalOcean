653a4dc7f2882564ca8013c142741161869900c8
Sarah7799 <82445581+Sarah7799@users.noreply.github.com>
2022-02-02 19:27:28 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ async function getData() {
     let resp = await fetch("https://wandbox.org/api/list.json");
     let data = await resp.json();
     createTable(data)
+    console.log(data);
     return data;
     }
     catch(error){
@@ -12,22 +13,21 @@ async function getData() {
 
 getData();
 
-
-
 function createTable(data){
+    console.log('data', data)
  let tBody = document.getElementById('tbody')
 
     data.forEach((element) => {
       let row = document.createElement('tr');
       let td1 = document.createElement("td");
-      td1.innerHTML = element.compiler-option-raw;
+      td1.innerHTML = element["name"];
       let td2 = document.createElement('td');
-      td2.innerHTML=element.runtime-option-raw;
+      td2.innerHTML=element["language"];
       let td3 = document.createElement('td');
-      td3.innerHTML = element.display-compile-command;
-      
-      row.append(td1,td2,td3);
+      td3.innerHTML = element["display-compile-command"];
+      let td4 = document.createElement('td');
+      td4.innerHTML = element.version;
+      row.append(td1,td2,td3,td4);
       tBody.append(row);
     });
 }
-
